780345966d358255070ac3f66963e391637b9009
Tanyiqu <1953649096@qq.com>
2020-04-19 19:05:29 +0800

    完成更新日志窗口

--- ui/Forms/LogsForm.py
+++ ui/Forms/LogsForm.py
@@ -1,5 +1,7 @@
-from PyQt5.QtWidgets import QWidget
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QWidget, QLabel
 
+import R
 from ui.ui_designer.ui_file.uic_logsForm import Ui_logsForm
 
 
@@ -10,6 +12,37 @@ class LogsForm(QWidget):
         self.logsForm = Ui_logsForm()
         self.logsForm.setupUi(self)
 
+        self.initAppearance()
+        self.initLogs()
+
+        pass
+
+    def initAppearance(self):
+        # 设置图标
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("resource/imgs/logo.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.setWindowIcon(icon)
+        pass
+
+    def initLogs(self):
+        for log in R.string.UPDATE_LOG:
+            text = '时间：' + log['time']
+            text += '\n版本：' + log['version']
+            text += '\n该版本总下载数：' + str(log['downloadNum'])
+            text += '\n更新详情：'
+            for detail in log['detail']:
+                text += '\n' + detail
+                pass
+            lbl = QLabel(text)
+            lbl.setFixedWidth(270)
+            lbl.setStyleSheet('font: 10pt "微软雅黑";'
+                              'padding: 3px;'
+                              'border: 1px solid #7f7f7f;')
+            lbl.setWordWrap(True)
+            lbl.adjustSize()
+            self.logsForm.layout.addWidget(lbl)
+            pass
+        self.logsForm.layout.addStretch()
         pass
 
     pass
