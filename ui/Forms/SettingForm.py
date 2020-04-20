93ad39c2152d23c0c2a356ce8f1922d67a1bdee6
Tanyiqu <1953649096@qq.com>
2020-04-20 14:32:07 +0800

    实现设置功能

--- ui/Forms/SettingForm.py
+++ ui/Forms/SettingForm.py
@@ -1,5 +1,12 @@
+import threading
+import webbrowser
+from subprocess import call
+
+from PyQt5 import QtGui
 from PyQt5.QtWidgets import QWidget
 
+import R
+from Configuration import Configuration
 from ui.ui_designer.ui_file.uic_settingForm import Ui_settingForm
 
 
@@ -9,4 +16,61 @@ class SettingForm(QWidget):
         self.parent = parent
         self.settingForm = Ui_settingForm()
         self.settingForm.setupUi(self)
+        self.config = Configuration()
+        self.initAppearance()
+        self.loadConfig()
+
+        self.settingForm.btnCheckUpdate.clicked.connect(lambda: webbrowser.open_new(R.string.DOWNLOAD_LINK))
+        self.settingForm.btnTestPlayer.clicked.connect(self._testPlayer)
+        self.settingForm.btnTestIDM.clicked.connect(self._testIDM)
+        self.settingForm.btnFinished.clicked.connect(self._finish)
+
+        pass
+
+    def initAppearance(self):
+        # 设置图标
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("resource/imgs/logo.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.setWindowIcon(icon)
+        pass
+
+    def _finish(self):
+        self.save()
+        self.close()
+        pass
+
+    def save(self):
+        print('点击完成')
+        self.config.user_name = self.settingForm.txtUserName.text().strip()
+        self.config.player_path = self.settingForm.lblPlayer.text().strip()
+        self.config.idm_path = self.settingForm.lblIDM.text().strip()
+        self.config.showClosingWarning = self.settingForm.checkClosingWarning.isChecked()
+        self.config.play_sound = self.settingForm.checkPlaySound.isChecked()
+        self.config.checkUpdate = self.settingForm.checkCheckUpdate.isChecked()
+        self.config.save()
+        pass
+
+    def _testPlayer(self):
+        path = self.settingForm.lblPlayer.text()
+        t = threading.Thread(target=lambda:call(path), name='testPlay')
+        t.start()
+        print('测试player', path)
+        pass
+
+    def _testIDM(self):
+        path = self.settingForm.lblIDM.text()
+        t = threading.Thread(target=lambda: call(path), name='testIDM')
+        t.start()
+        print('测试idm', path)
+        pass
+
+    def loadConfig(self):
+        self.settingForm.txtUserName.setText(self.config.user_name)
+        self.settingForm.lblPlayer.setText(self.config.player_path)
+        self.settingForm.lblIDM.setText(self.config.idm_path)
+        self.settingForm.checkClosingWarning.setChecked(self.config.showClosingWarning)
+        self.settingForm.checkPlaySound.setChecked(self.config.play_sound)
+        self.settingForm.checkCheckUpdate.setChecked(self.config.checkUpdate)
+        pass
+
     pass
