c2a6de137f6d99ca4eaea00382a8bb0bdeb53d34
Tanyiqu <1953649096@qq.com>
2020-04-29 10:09:34 +0800

    修改设置对话框

--- ui/Forms/SettingForm.py
+++ ui/Forms/SettingForm.py
@@ -22,14 +22,14 @@ class SettingForm(QWidget):
 
         self.settingForm.btnCheckUpdate.clicked.connect(lambda: webbrowser.open_new(R.string.DOWNLOAD_LINK))
 
-        self.settingForm.btnTestPlayer.clicked.connect(self._testPlayer)
-        self.settingForm.btnTestPlayer2.clicked.connect(self._testPlayer2)
+        self.settingForm.btnTestPlayerPot.clicked.connect(self._testPlayer)
+        self.settingForm.btnTestPlayerVlc.clicked.connect(self._testPlayer2)
         self.settingForm.btnTestIDM.clicked.connect(self._testIDM)
 
-        self.settingForm.btnFinished.clicked.connect(self._finish)
+        self.settingForm.btnFinished_1.clicked.connect(self._finish)
 
-        self.settingForm.btnChoosePlayer.clicked.connect(self._choosePlayer)
-        self.settingForm.btnChoosePlayer2.clicked.connect(self._choosePlayer2)
+        self.settingForm.btnChoosePlayerPot.clicked.connect(self._choosePlayer)
+        self.settingForm.btnChoosePlayerVlc.clicked.connect(self._choosePlayer2)
         self.settingForm.btnChooseIDM.clicked.connect(self._chooseIDM)
 
         pass
@@ -50,8 +50,8 @@ class SettingForm(QWidget):
     def save(self):
         print('点击完成')
         self.config.user_name = self.settingForm.txtUserName.text().strip()
-        self.config.player_pot_path = self.settingForm.lblPlayer.text().strip()
-        self.config.player_vlc_path = self.settingForm.lblPlayer2.text().strip()
+        self.config.player_pot_path = self.settingForm.lblPlayerPot.text().strip()
+        self.config.player_vlc_path = self.settingForm.lblPlayerVlc.text().strip()
         self.config.idm_path = self.settingForm.lblIDM.text().strip()
         self.config.showClosingWarning = self.settingForm.checkClosingWarning.isChecked()
         self.config.play_sound = self.settingForm.checkPlaySound.isChecked()
@@ -60,14 +60,14 @@ class SettingForm(QWidget):
         pass
 
     def _testPlayer(self):
-        path = self.settingForm.lblPlayer.text()
+        path = self.settingForm.lblPlayerPot.text()
         t = threading.Thread(target=lambda: call(path), name='testPlay')
         t.start()
         print('测试player', path)
         pass
 
     def _testPlayer2(self):
-        path = self.settingForm.lblPlayer2.text()
+        path = self.settingForm.lblPlayerVlc.text()
         t = threading.Thread(target=lambda: call(path), name='testPlay')
         t.start()
         print('测试player', path)
@@ -86,7 +86,7 @@ class SettingForm(QWidget):
                                                                 './',
                                                                 "播放器 (*exe);")
         if fileName_choose != '':
-            self.settingForm.lblPlayer.setText(fileName_choose)
+            self.settingForm.lblPlayerPot.setText(fileName_choose)
         pass
 
     def _choosePlayer2(self):
@@ -95,7 +95,7 @@ class SettingForm(QWidget):
                                                                 './',
                                                                 "播放器 (*exe);")
         if fileName_choose != '':
-            self.settingForm.lblPlayer2.setText(fileName_choose)
+            self.settingForm.lblPlayerVlc.setText(fileName_choose)
         pass
 
     def _chooseIDM(self):
@@ -109,8 +109,8 @@ class SettingForm(QWidget):
 
     def loadConfig(self):
         self.settingForm.txtUserName.setText(self.config.user_name)
-        self.settingForm.lblPlayer.setText(self.config.player_pot_path)
-        self.settingForm.lblPlayer2.setText(self.config.player_vlc_path)
+        self.settingForm.lblPlayerPot.setText(self.config.player_pot_path)
+        self.settingForm.lblPlayerVlc.setText(self.config.player_vlc_path)
         self.settingForm.lblIDM.setText(self.config.idm_path)
         self.settingForm.checkClosingWarning.setChecked(self.config.showClosingWarning)
         self.settingForm.checkPlaySound.setChecked(self.config.play_sound)
